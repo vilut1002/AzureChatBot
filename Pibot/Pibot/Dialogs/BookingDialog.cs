cb9cb5a24d70969f0904a993bc4d4bc8ac5e0635
dahaelee <53745427+dahaelee@users.noreply.github.com>
2020-07-16 18:26:43 +0900

    age

--- Pibot/Pibot/Dialogs/BookingDialog.cs
+++ Pibot/Pibot/Dialogs/BookingDialog.cs
@@ -69,40 +69,41 @@ namespace Pibot.Dialogs
 
         private async Task<DialogTurnResult> NotesStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            var notes = $"※ 헌혈 예약 시 유의사항 ※{System.Environment.NewLine}";
-            notes += $"- 6개월 후까지 예약할 수 있으며, 당일 예약은 불가합니다.{System.Environment.NewLine}";
-            notes += $"- 최근 헌혈혈액검사에 따라 헌혈이 제한될 수 있습니다.{System.Environment.NewLine}";
-            notes += $"- 예약시간 경과 시 예약이 취소되니 주의해 주십시오.{System.Environment.NewLine}";
-            notes += $"- 헌혈의 집 도착 시 예약헐혈자임을 직원에게 말씀해 주십시오.{System.Environment.NewLine}";
+            var notes = $"※ 헌혈 예약 시 유의사항 ※\r\n";
+            notes += $"- 6개월 후까지 예약할 수 있으며, 당일 예약은 불가합니다.\r\n";
+            notes += $"- 최근 헌혈혈액검사에 따라 헌혈이 제한될 수 있습니다.\r\n";
+            notes += $"- 예약시간 경과 시 예약이 취소되니 주의해 주십시오.\r\n";
+            notes += $"- 헌혈의 집 도착 시 예약헐혈자임을 직원에게 말씀해 주십시오.\r\n";
             await stepContext.Context.SendActivityAsync(MessageFactory.Text(notes), cancellationToken);
 
             var activity = new Attachment[]
-                      {
-                                        new HeroCard(
-                                            title: "신체조건",
-                                            images: new CardImage[] { new CardImage() { Url = "https://www.bloodinfo.net/image/character_img.png" } },
-                                            buttons: new CardAction[]
-                                            {
-                                                new CardAction(title: "열기", type: ActionTypes.OpenUrl, value: "https://www.bloodinfo.net/image/character_img.png")
-                                            })
-                                        .ToAttachment(),
-                                        new HeroCard(
-                                            title: "약물",
-                                            images: new CardImage[] { new CardImage() { Url = "https://www.bloodinfo.net/image/character_img.png" } },
-                                            buttons: new CardAction[]
-                                            {
-                                                new CardAction(title: "열기", type: ActionTypes.OpenUrl, value: "https://www.bloodinfo.net/image/character_img.png")
-                                            })
-                                        .ToAttachment(),
-                                        new HeroCard(
-                                            title: "어쩌구",
-                                            images: new CardImage[] { new CardImage() { Url = "https://www.bloodinfo.net/image/character_img.png" } },
-                                            buttons: new CardAction[]
-                                            {
-                                                new CardAction(title: "열기", type: ActionTypes.OpenUrl, value: "https://www.bloodinfo.net/image/character_img.png")
-                                            })
-                                        .ToAttachment()
-                      };
+            {
+                new HeroCard(
+                    title: "신체조건",
+                    images: new CardImage[] { new CardImage() { Url = "https://www.bloodinfo.net/image/character_img.png" } },
+                    buttons: new CardAction[]
+                    {
+                        new CardAction(title: "열기", type: ActionTypes.OpenUrl, value: "https://www.bloodinfo.net/image/character_img.png")
+                    })
+                .ToAttachment(),
+                new HeroCard(
+                    title: "약물",
+                    images: new CardImage[] { new CardImage() { Url = "https://www.bloodinfo.net/image/character_img.png" } },
+                    buttons: new CardAction[]
+                    {
+                        new CardAction(title: "열기", type: ActionTypes.OpenUrl, value: "https://www.bloodinfo.net/image/character_img.png")
+                    })
+                .ToAttachment(),
+                new HeroCard(
+                    title: "어쩌구",
+                    images: new CardImage[] { new CardImage() { Url = "https://www.bloodinfo.net/image/character_img.png" } },
+                    buttons: new CardAction[]
+                    {
+                        new CardAction(title: "열기", type: ActionTypes.OpenUrl, value: "https://www.bloodinfo.net/image/character_img.png")
+                    })
+                .ToAttachment()
+            };
+
             var reply = MessageFactory.Attachment(activity);
             reply.Attachments = activity;
             reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
@@ -150,13 +151,21 @@ namespace Pibot.Dialogs
 
         private static Task<bool> AgePromptValidatorAsync(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
         {
-            return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 0 && promptContext.Recognized.Value < 150);
+            return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 0);
         }
 
         private async Task<DialogTurnResult> PhoneStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            stepContext.Values["age"] = (int)stepContext.Result;
-            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("연락처를 입력해주세요.") }, cancellationToken);
+            if ((int)stepContext.Result >= 16 && (int)stepContext.Result <= 69)
+            {
+                stepContext.Values["age"] = (int)stepContext.Result;
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("연락처를 입력해주세요.") }, cancellationToken);
+            }
+            else
+            {
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text($"죄송합니다. 나이가 16세 미만, 69세 초과일 경우 헌혈을 하실 수 없습니다."), cancellationToken);
+                return await stepContext.EndDialogAsync(null, cancellationToken);
+            }
         }
 
         private async Task<DialogTurnResult> HouseChoiceStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
@@ -166,11 +175,11 @@ namespace Pibot.Dialogs
             return await stepContext.PromptAsync(nameof(ChoicePrompt),
             new PromptOptions
             {
-                Prompt = MessageFactory.Text("어느 지점으로 방문하시겠어요?"),
+                Prompt = MessageFactory.Text("방문하실 헌혈의 집 센터를 선택해주세요."),
                 Choices = ChoiceFactory.ToChoices(new List<string> { "신촌점", "홍대점", "합정점" }),
             }, cancellationToken);
         }
-    
+
 
         private async Task<DialogTurnResult> DateStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
@@ -195,10 +204,10 @@ namespace Pibot.Dialogs
             bookingDetails.Date = (string)stepContext.Values["date"];
 
             var bookingResult = $"{userProfile.Name}님의 예약 정보입니다.{System.Environment.NewLine}";
-            bookingResult += $"성별 : {userProfile.Sex}{System.Environment.NewLine}";
-            bookingResult += $"나이 : {userProfile.Age}{System.Environment.NewLine}";
-            bookingResult += $"연락처 : {userProfile.Phone}{System.Environment.NewLine}";
-            bookingResult += $"지점 : {bookingDetails.House}{System.Environment.NewLine}";
+            bookingResult += $"성별 : {userProfile.Sex}\r\n";
+            bookingResult += $"나이 : {userProfile.Age}\r\n";
+            bookingResult += $"연락처 : {userProfile.Phone}\r\n";
+            bookingResult += $"지점 : {bookingDetails.House}\r\n";
             bookingResult += $"날짜 : {bookingDetails.Date}";
             await stepContext.Context.SendActivityAsync(MessageFactory.Text(bookingResult), cancellationToken);
 
@@ -221,6 +230,5 @@ namespace Pibot.Dialogs
         }
 
     }
-
 
 }
